609c4b22f1d86e84f50c6e7a5339e1261de0b7e5
Om Mishra <contact@om-mishra.com>
2024-04-07 08:06:24 +0530

    Support Pages

--- application/server.py
+++ application/server.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template("public/home.html")
 
+@app.route("/support")
+def support():
+    return render_template("public/support.html")
+
+@app.route("/terms-of-service")
+def tos():
+    return render_template("public/tos.html")
+
+@app.route("/privacy-policy")
+def privacy():
+    return render_template("public/privacy-policy.html")
+
+@app.route("/cancellation")
+def cancellation():
+    return render_template("public/cancellation.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
