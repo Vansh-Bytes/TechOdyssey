311d399090bf7fedcdb031e1e8ceb99deb234588
Om Mishra <contact@om-mishra.com>
2024-04-08 06:30:11 +0530

    Requirements Updated

--- application/server.py
+++ application/server.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from pymongo import MongoClient
 from flask_session import Session
 from flask_social_oauth import Config, initialize_social_login
-from flask import Flask, render_template, redirect, url_for, request, session, jsonify
+from flask import Flask, render_template, redirect, url_for, session, jsonify
 
 # Helper functions
 
